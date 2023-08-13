f590bd21016b40668ca87e782230f3019cd593ef
Cameron <805camerons@gmail.com>
2023-08-12 20:32:50 -0400

    Create GameConfig.cs

---
+++ code/GameConfig.cs
@@ -0,0 +1,20 @@
+﻿using Sandbox;
+
+namespace PlatesGame;
+
+public static class GameConfig
+{
+	[ConVar.Replicated( "min_players" )] 
+	public static int MinimumPlayers { get; set; } = 2;
+
+	[ConVar.Replicated( "time_between_rounds" )]
+	public static float TimeBetweenRounds { get; set; } = 10f;
+
+	[ConVar.Replicated( "time_winner_screen" )]
+	public static float WinnerScreenTime { get; set; } = 20f;
+
+	[ConVar.Replicated( "player_default_gravity" )]
+	public static float DefaultGravity { get; set; } = 1000f;
+
+}
+
