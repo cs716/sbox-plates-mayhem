5dbab46027648f87577a09ec04b9c72c6d4d6b8e
Cameron <805camerons@gmail.com>
2023-09-12 20:55:33 -0400

    Update BarrelRainEvent.cs
    Barrels fall faster
    Changed model for barrels

--- code/Event/Events/Arena/BarrelRainEvent.cs
+++ code/Event/Events/Arena/BarrelRainEvent.cs
@@ -47,19 +47,18 @@ public class BarrelRainEvent : BaseEvent
 
 		var barrel = new BarrelEntity
 		{
-			Position = new Vector3( Random.Shared.Int(-1500,1500), Random.Shared.Int(-1500,1500), 1000 ),
+			Position = new Vector3( Random.Shared.Int(-1500,1500), Random.Shared.Int(-1500,1500), 5000 ),
 			Rotation = Rotation.From(new Angles(Random.Shared.Float()*360,Random.Shared.Float()*360,Random.Shared.Float()*360)),
-			Velocity = new Vector3(0, 0, -100),
-			Scale = 2f,
+			Velocity = new Vector3(0, 0, -500),
+			Scale = 1f,
 			Name = "Explosive Barrel",
 			Health = 1f
 		};
 		if ( slamPlayerChance == 1 ) // Unlucky, Slam that shit on someones head
 			barrel.Position = (Vector3)Entity.All.OfType<PlatesPlayer>().Where( p => p.LifeState == LifeState.Alive )
-				.OrderBy( x => Random.Shared.Double( 1, 100 ) ).First()?.Position.WithZ( 5000 );
-		barrel.SetModel("models/sbox_props/oil_drum/oil_drum_explosive.vmdl_c");
+				.OrderBy( x => Random.Shared.Double( 1, 100 ) ).First().Position.WithZ( 5000 );
+		barrel.SetModel("models/red_barrel.vmdl_c");
 		barrel.Tags.Add("eventProp");
 		barrel.Spawn();
-		Log.Info( "Spawned barrel" );
 	}
 }
