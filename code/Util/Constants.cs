18ac842a25e8866ea85df258d677048b2af8a85f
Cameron <805camerons@gmail.com>
2023-09-15 19:26:19 -0400

    Add text chat (and host controls) to Lobby

---
+++ code/Util/Constants.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Sandbox;
+
+namespace PlatesGame;
+
+public static class Stat
+{
+	public static string Wins = "wins";
+	public static string Deaths = "deaths";
+	public static string EventsSurvived = "events";
+}
+
+public static class Players
+{
+	public static IEnumerable<PlatesPlayer> GetLiving()
+	{
+		return Entity.All.OfType<PlatesPlayer>().Where( p => p.LifeState == LifeState.Alive );
+	}
+	
+}
